c7d55242269a4c5ac4336723ac28da5b0e02a387
Magnusson95 <alexander.magnusson95@gmail.com>
2020-08-10 16:39:35 +0000

    Added additional requirements. Preparing to commit to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -74,12 +75,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.parse('postgres://onhbwcbpexurpg:0eb24361d992c7916fe4e867612b2db7c5f7a0b0c73703e1494cfb86b1d72933@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/de1887khq311sq')
     }
-}
 
 
 # Password validation
